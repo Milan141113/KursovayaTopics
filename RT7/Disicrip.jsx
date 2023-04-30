1e8f5221ca75e91bd439f91c4bd4f1b4c84dd147
Milan141113 <79976666+Milan141113@users.noreply.github.com>
2023-04-30 20:00:27 +0300

    Add files via upload

---
+++ RT7/Disicrip.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Disicrip() {
+  return (
+    <div className='discribe'>
+        Добро пожаловать на наш сайт конвертера валют! Мы рады предоставить вам удобный и точный инструмент для конвертации валют.
+    </div>
+  )
+}
